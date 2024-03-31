96fa4158f7df83f74029eb965365c36d6af861df
vikas-saini-7 <vikas289888@gmail.com>
2024-03-31 14:26:10 +0530

    tools changed

--- client/src/components/EditorImageAreaFooter.jsx
+++ client/src/components/EditorImageAreaFooter.jsx
@@ -9,7 +9,7 @@ const EditorImageAreaFooter = () => {
             <button className='px-4 py-1 rounded bg-gray-100 hover:bg-gray-200'>Fit Screen</button>
             <div className='flex items-center gap-2'>
               <p className='text-xs'>0%</p>
-              <input className="appearance-none cursor-pointer w-[200px] h-1 rounded-full bg-gray-200 outline-none focus:outline-none  focus:bg-gray-300 focus:ring-2 focus:ring-0" type="range" name="" id="" />
+              <input className="appearance-none cursor-pointer w-[200px] h-1 rounded-full bg-gray-200 outline-none focus:outline-none  focus:bg-gray-300 focus:ring-0" type="range" name="" id="" />
               <p className='text-xs'>100%</p>
             </div>
         </div>
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,32 +1,52 @@
 export const tools = [
     {
-        id: 1,
-        name: 'Beautify',
-        icon: 'image',
+        id: 0,
+        name: 'Create New',
+        icon: 'plus-circle',
         link: 'beautify'
     },
     {
+        id: 1,
+        name: 'ai Beautify',
+        icon: 'image',
+        link: 'beautify',
+        ai: true
+    },
+    {
         id: 2,
-        name: 'Crop',
+        name: 'Tools',
         icon: 'crop',
         link: 'crop'
     },
     {
         id: 3,
-        name: 'Filter',
-        icon: 'image',
-        link: 'filter'
+        name: 'Elements',
+        icon: 'square',
+        link: 'elements'
     },
     {
         id: 4,
-        name: 'Adjust',
+        name: 'Filter',
         icon: 'image',
-        link: 'adjust'
+        link: 'filter'
     },
     {
         id: 5,
         name: 'Upload',
         icon: 'upload-cloud',
         link: 'adjust'
+    },
+    {
+        id: 6,
+        name: 'Stock',
+        icon: 'image',
+        link: 'stock'
+    },
+    {
+        id: 7,
+        name: 'AI generated',
+        icon: 'image',
+        link: 'ai-stock',
+        ai: true
     },
 ]
